d3158668b9fd4537a6a136101f29b7b3b8c56fd0
sejaljksol <sejal.jksol@gmail.com>
2026-03-12 10:35:16 +0530

    Add files via upload

---
+++ Sources/SampleFramework/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  SampleFramework
+//
+//  Created by SEJAL on 12/03/26.
+//
+
+public class HelloWorld {
+
+    public init() {}
+
+    public func printMessage(msg: String) {
+        print(msg)
+    }
+
+}
